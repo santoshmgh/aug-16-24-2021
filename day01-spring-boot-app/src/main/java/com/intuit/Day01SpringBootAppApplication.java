71795a6b2eef0c97b22246e1fc8b7bd5a02d5999
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-08-16 17:20:10 +0530

    Day01

---
+++ day01-spring-boot-app/src/main/java/com/intuit/Day01SpringBootAppApplication.java
@@ -0,0 +1,27 @@
+package com.intuit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01SpringBootAppApplication 
+		implements CommandLineRunner {
+
+	@Autowired
+	private Company company;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Day01SpringBootAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("****" + company.getName());
+		
+	}
+	
+	
+
+}
---
+++ day01-spring-boot-app/src/test/java/com/intuit/Day01SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

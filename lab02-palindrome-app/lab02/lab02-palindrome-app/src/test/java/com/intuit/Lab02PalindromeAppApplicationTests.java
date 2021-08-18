d63aa8f654e97fe7826c63fe72154f305cd84a5e
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-08-18 14:52:36 +0530

    Day03

---
+++ lab02-palindrome-app/lab02/lab02-palindrome-app/src/test/java/com/intuit/Lab02PalindromeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab02PalindromeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
